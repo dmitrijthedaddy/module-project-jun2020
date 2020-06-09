6c0c45b9376ae4b02715e069639bd2373c4fc07e
dmitrijthedaddy <demitya7@gmail.com>
2020-06-09 14:06:17 +0500

    IntegrationMethods: пофикшен метод Simpson2Method + комменты

--- function-integration/IntegrationMethods.cs
+++ function-integration/IntegrationMethods.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace function_integration
 {
     public abstract class IntegrationMethods
-    { // Методы: средних прямоугольников, трапеций, Симпсона 3/8
-        abstract public double Y(double x);
+    { // методы: средних прямоугольников, трапеций, Симпсона 3/8
 
+        // абстрактные функции: f(x) и F(x)
+        abstract public double Y(double x);
         abstract public double IntY(double low, double hi);
 
         public double MidRectMethod(double low, double intStep, double stepCount)
-        {
+        { // интегрирование методом трапеций
             double result = 0, x = low;
 
             for (int i = 1; i < stepCount; i++)
@@ -26,7 +23,7 @@ namespace function_integration
         }
 
         public double TrapeziaMethod(double low, double intStep, double stepCount) 
-        {
+        { // интегрирование методом трапеций
             double result = 0, x = low;
 
             for (int i = 1; i < stepCount; i++)
@@ -39,15 +36,21 @@ namespace function_integration
         }
 
         public double Simspon2Method(double low, double intStep, double stepCount)
-        {
+        { // метод для интегрирования методом Симпсона 3/8
             double result = 0, x = low;
 
             for (int i = 1; i < stepCount; i++)
             {
+                /* 
+                    для удобства числитель разбит на 
+                    три дополнительные переменные
+                */
                 double a = Y(x - intStep);
                 double b = 3 * Y((2 * (x - intStep) + x) / 3);
                 double c = 3 * Y((x - intStep + 2 * x) / 3);
+
                 result += (a + b + c + Y(x)) / 8 * intStep;
+                x += intStep;
             }
 
             return result;
