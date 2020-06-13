d6aa7088a6abe83fcacfee4fe1f8ffdc445a0f2c
dmitrijthedaddy <demitya7@gmail.com>
2020-06-14 01:17:36 +0500

    ArrayKit: добавлен дочерний класс со случайной генерацией элементов

--- one-dim-array/ArrayKit.cs
+++ one-dim-array/ArrayKit.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace one_dim_array
 {
-    class CustomArray
+    class CustomArrayBase
     {
         protected int[] array;
         protected int Size { get; set; }
         public string SortMode { get; private set; }
 
-        public CustomArray()
+        public CustomArrayBase()
         {
             array = new int[] {5, 8, 10, 12, 24, 11, 9, 5, 8, 1};
             Size = array.Length;
@@ -35,6 +35,32 @@ namespace one_dim_array
             }
 
             return neighbouringSum;
+        }
+    }
+
+    class CustomArray : CustomArrayBase
+    {
+        private int lowerBound, upperBound;
+
+        public CustomArray(int size, int low, int high)
+        {
+            Size = size;
+            lowerBound = low;
+            upperBound = high;
+            array = RandomArray();
+        }
+
+        private int[] RandomArray()
+        {
+            var random = new Random();
+            int[] randomArray = new int[Size];
+
+            for (var i = 0; i < Size; i++)
+            {
+                randomArray[i] = random.Next(lowerBound, upperBound + 1);
+            }
+
+            return randomArray;
         }
     }
 }
