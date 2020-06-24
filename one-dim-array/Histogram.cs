0ae5764bbdd20a9030ceb48899dc3cbbc6b4eafc
dmitrijthedaddy <demitya7@gmail.com>
2020-06-24 14:55:27 +0500

    Histogram: убраны ненужные using'и

--- one-dim-array/Histogram.cs
+++ one-dim-array/Histogram.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace one_dim_array
+﻿namespace one_dim_array
 {
     static class Histogram
     {
